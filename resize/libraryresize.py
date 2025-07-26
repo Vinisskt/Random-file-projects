fbbebfc9ecdd7f6c1db2416998ce80d2948d1470
vinicius <vinissktmaloca@gmail.com>
2025-07-26 11:12:07 -0300

    Refatora: separa funções em libraryresize.py e mantém main em resize.py

--- resize/libraryresize.py
+++ resize/libraryresize.py
@@ -1,26 +1,14 @@
-import sys
 import random
 from PIL import Image
+import sys
 
-def main():
-  if len(sys.argv) != 4:
-    print("python namecrypt image.jpg height width ")
-    sys.exit(1)
-  
-  if checkfile(sys.argv[1]) == False:
-    print("file not in format .jpg")
-    sys.exit(1)
-  
-  newsize = (int(sys.argv[2]),int(sys.argv[3]))
-  
-  choice = input("enter Y or N").upper()
-  
+def resizeimage(file, newsize, choice):
   filename = rename(choice)
-  imageinput = Image.open(sys.argv[1])
+  imageinput = Image.open(file)
   imageoutput = imageinput.resize(newsize)
   imageoutput.save(filename)
   print(filename + " saved sucessfully")
-
+  
 def namerandom(array):
   randomname = ""
   for i in range(5):
@@ -35,15 +23,19 @@ def rename(choice):
   if choice == "N":
     array = ["a","e","u","k","m"]
     filename = namerandom(array) + ".jpg"
-    
   return filename
     
-def checkfile(file):
+def checkfile(file, imageformat):
   checkformat = False
   check = file[-3:]
-  if str(check) == "jpg":
+  if str(check) == imageformat:
     checkformat = True
   return checkformat
-
-if __name__ == '__main__':
-  main()
+  
+def readfiletxt(txtfile):
+  arraytxt = []
+  with open(txtfile, 'r') as filetxt:
+    for text in filetxt:
+      text = text.strip()
+      arraytxt.append(str(text))
+    return arraytxt
--- resize/resize.py
+++ resize/resize.py
@@ -1,50 +1,24 @@
 import sys
-import random
-from PIL import Image
+import libraryresize
 
 def main():
   if len(sys.argv) != 4:
-    print("python namecrypt image.jpg height width ")
+    print("python namecrypt <image.jpg> or <file.txt> <height> <width> ")
     sys.exit(1)
-  
-  if checkfile(sys.argv[1]) == False:
-    print("file not in format .jpg")
-    sys.exit(1)
-  
+
   newsize = (int(sys.argv[2]),int(sys.argv[3]))
   
-  choice = input("enter Y or N").upper()
-  
-  filename = rename(choice)
-  imageinput = Image.open(sys.argv[1])
-  imageoutput = imageinput.resize(newsize)
-  imageoutput.save(filename)
-  imageoutput.save(filename)
-  print(filename + " saved sucessfully")
-
-def namerandom(array):
-  randomname = ""
-  for i in range(5):
-    randomname += random.choice(array)
-  return randomname
-
-def rename(choice):
-  filename = ""
-  if choice == "Y":
-    newname = input("enter a name no jpg")
-    filename = newname + ".jpg"
-  if choice == "N":
-    array = ["a","e","u","k","m"]
-    filename = namerandom(array) + ".jpg"
+  if libraryresize.checkfile(sys.argv[1], "txt") == True:
+    arraytxt = libraryresize.readfiletxt(sys.argv[1])
+    for txt in arraytxt:
+      if libraryresize.checkfile(str(txt), "jpg") == False:
+        print("file not in format .jpg")
+        sys.exit(1)
+      libraryresize.resizeimage(txt, newsize , choice="N")
+      
+  if libraryresize.checkfile(sys.argv[1], "jpg") == True:
+    choice = input("enter Y or N").upper()
+    libraryresize.resizeimage(sys.argv[1], newsize, choice)
     
-  return filename
-    
-def checkfile(file):
-  checkformat = False
-  check = file[-3:]
-  if str(check) == "jpg":
-    checkformat = True
-  return checkformat
-
 if __name__ == '__main__':
   main()
